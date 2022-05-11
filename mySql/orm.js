e8d30fc1052b2da85dc0908d63d3718dd493577a
Trevor Olson <trevor.j.olson1@gmail.com>
2022-05-11 14:26:31 -0700

    "similar products" added
    added a similar products feature at the bottom of the individual product pages

--- mySql/orm.js
+++ mySql/orm.js
@@ -5,182 +5,177 @@ const connection = require('./connection');
 
 const orm = {
     selectAll(cb) {
-        connection.query("SELECT * FROM products", function(err, data){
-            if(err){
+        connection.query("SELECT * FROM products", function (err, data) {
+            if (err) {
                 cb(err, null);
             }
             cb(null, data);
         });
     },
-    selectAllFiltered( category, type, cb )
-    {
+    selectAllFiltered(category, type, cb) {
         let sqlQuery = 'SELECT * FROM products';
-        if( category && type )
-        {
+        if (category && type) {
             sqlQuery = `SELECT * FROM products WHERE category = "${category}"
                 AND type = "${type}";`;
         }
-        else if( category )
-        {
+        else if (category) {
             sqlQuery = `SELECT * FROM products WHERE category = "${category}";`;
         }
-        else if( type )
-        {
+        else if (type) {
             sqlQuery = `SELECT * FROM products WHERE type = "${type}";`;
         }
-        connection.query( sqlQuery, function(err, data){
-            if(err){
+        connection.query(sqlQuery, function (err, data) {
+            if (err) {
                 cb(err, null);
             }
             cb(null, data);
         });
     },
-    async getProduct( id, cb ) {
+    getProduct(id, cb) {
         const sqlQuery = `SELECT * FROM products WHERE product_id = ${id};`;
-        connection.query( sqlQuery, function(err, data) {
-            if(err){
+        connection.query(sqlQuery, function (err, data) {
+            if (err) {
                 cb(err, null);
             }
-            if( data[0].type == 'Clothing' )
-            {
-                // get the sizes
-                const sizeQuery = `SELECT s.* FROM sizes as s 
-                JOIN products as p ON s.product_id = p.product_id
-                WHERE p.product_id = ${id};`;
-                connection.query( sizeQuery, function(err, sizeData) {
-                    if(err){
-                        cb(err, null);
-                    }
-                    data[0]['sizes'] = sizeData[0];
-                    // get the colors
-                    const colorQuery = `SELECT c.color_id, c.color_name FROM colors as c 
-                    JOIN products as p ON c.product_id = p.product_id
-                    WHERE p.product_id = ${id};`;
-                    connection.query( colorQuery, function(err, colorData) {
-                        if(err){
+            // get the similar products
+            const simQuery = `SELECT product_id, name, category, type, image FROM products 
+                            WHERE product_id != ${id}
+                            ORDER BY FIELD( category, "${data[0].category}") DESC, 
+                                     FIELD( type, "${data[0].type}") DESC
+                            LIMIT 5;`;
+            connection.query(simQuery, function (err, simData) {
+                if (err) {
+                    cb(err, null);
+                }
+                data[0]['similarProducts'] = simData;
+                if (data[0].type == 'Clothing') {
+                    // get the sizes
+                    const sizeQuery = `SELECT s.* FROM sizes as s 
+                        JOIN products as p ON s.product_id = p.product_id
+                        WHERE p.product_id = ${id};`;
+                    connection.query(sizeQuery, function (err, sizeData) {
+                        if (err) {
                             cb(err, null);
                         }
-                        data[0]['colors'] = {}
-                        for( d in colorData )
-                        {
-                            data[0]['colors'][colorData[d].color_id] = colorData[d].color_name;
-                        }
-                        cb( null, data )
+                        data[0]['sizes'] = sizeData[0];
+                        // get the colors
+                        const colorQuery = `SELECT c.color_id, c.color_name 
+                            FROM colors as c 
+                            JOIN products as p ON c.product_id = p.product_id
+                            WHERE p.product_id = ${id};`;
+                        connection.query(colorQuery, function (err, colorData) {
+                            if (err) {
+                                cb(err, null);
+                            }
+                            data[0]['colors'] = {}
+                            for (d in colorData) {
+                                data[0]['colors'][colorData[d].color_id] = colorData[d].color_name;
+                            }
+                            cb(null, data)
+                        })
                     })
-                })
-            }
-            else
-            {
-                cb( null, data )
-            }
+                }
+                else {
+                    cb(null, data)
+                }
+            })
         });
     },
-    addToCart( userid, productid, qty = 1, size = null, color = null ) {
+    addToCart(userid, productid, qty = 1, size = null, color = null) {
         // check if product is in the cart
         let sqlQuery = '';
         // if it doesn't have a size or a color change the query
-        let clothing = ( size != null && color != null );
-        if( clothing )
-        {
+        let clothing = (size != null && color != null);
+        if (clothing) {
             sqlQuery = `SELECT quantity FROM cart 
                             WHERE user_id = ${userid} 
                             AND product_id = ${productid}
                             AND size = "${size}"
                             AND color_id = ${color};`;
-            
+
         }
-        else
-        {
+        else {
             sqlQuery = `SELECT quantity FROM cart 
                             WHERE user_id = ${userid} 
                             AND product_id = ${productid};`;
         }
-        connection.query( sqlQuery, function(err, data) {
-            if( err )
-            {
-                console.log( err );
+        connection.query(sqlQuery, function (err, data) {
+            if (err) {
+                console.log(err);
             }
-            if( data.length === 0 )
-            {
+            if (data.length === 0) {
                 sqlQuery = `INSERT INTO cart ( user_id, product_id, quantity, 
                     size, color_id ) VALUES
                     ( ${userid}, ${productid}, ${qty}, "${size}", ${color} );`
-                connection.query( sqlQuery );
+                connection.query(sqlQuery);
             }
-            else{
+            else {
                 let quantity = qty + data[0].quantity;
-                if( clothing )
-                {
+                if (clothing) {
                     sqlQuery = `UPDATE cart SET quantity = ${quantity} 
                                 WHERE user_id = ${userid} 
                                 AND product_id = ${productid}
                                 AND size = "${size}"
                                 AND color_id = ${color};`
                 }
-                else
-                {
+                else {
                     sqlQuery = `UPDATE cart SET quantity = ${quantity} 
                                 WHERE user_id = ${userid} 
                                 AND product_id = ${productid};`
                 }
-                
-                connection.query( sqlQuery );
+
+                connection.query(sqlQuery);
             }
         });
     },
-    removeFromCart( userid, productid, qty = 1, size = null, color = null ) {
+    removeFromCart(userid, productid, qty = 1, size = null, color = null) {
         // check if product is in the cart
         let sqlQuery = '';
         // if it doesn't have a size or a color change the query
-        let clothing = ( size != null && color != null );
-        if( clothing )
-        {
+        let clothing = (size != null && color != null);
+        if (clothing) {
             sqlQuery = `SELECT quantity FROM cart 
                         WHERE user_id = ${userid} 
                         AND product_id = ${productid}
                         AND size = "${size}"
                         AND color_id = ${color};`;
-            
+
         }
-        else
-        {
+        else {
             sqlQuery = `SELECT quantity FROM cart 
                         WHERE user_id = ${userid} 
                         AND product_id = ${productid};`;
         }
-        connection.query( sqlQuery, function(err, data) {
-            if( data.length === 0 )
-            { 
+        connection.query(sqlQuery, function (err, data) {
+            if (data.length === 0) {
                 return;
             }
-            else{
-                if( clothing )
-                {
+            else {
+                if (clothing) {
                     sqlQuery = `DELETE FROM cart 
                                 WHERE user_id = ${userid} 
                                 AND product_id = ${productid}
                                 AND size = "${size}"
                                 AND color_id = ${color};`
                 }
-                else
-                {
+                else {
                     sqlQuery = `DELETE FROM cart 
                                 WHERE user_id = ${userid} 
                                 AND product_id = ${productid};`
                 }
-                connection.query( sqlQuery );
+                connection.query(sqlQuery);
             }
         });
     },
-    getCart( userid, cb ){
+    getCart(userid, cb) {
         const sqlQuery = `SELECT p.name, p.category, p.type, p.image, p.price, c.*, 
             p.price * c.quantity AS "totalprice", colors.color_name
             FROM cart c
             JOIN products p ON p.product_id = c.product_id
             LEFT JOIN colors ON c.color_id = colors.color_id
             WHERE c.user_id = ${userid};`;
-        connection.query( sqlQuery, function(err, data){
-            if(err){
+        connection.query(sqlQuery, function (err, data) {
+            if (err) {
                 cb(err, null);
             }
             cb(null, data);
